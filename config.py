26a0b81c4e8537ab40f315d55a6254d660ae2540
Esko Hanell <esko.hanell@visma.com>
2021-09-06 14:06:37 +0300

    most recent changes

--- config.py
+++ config.py
@@ -42,6 +42,7 @@ from xkeysnail.transform import *
 define_keymap(re.compile("jetbrains-idea"), {
     # K("M-KEY_102ND"): K("C-home"),
     # K("M-LShift-KEY_102ND"): K("C-end"),
+
     K("C-a"): with_mark(K("home")),
     K("C-e"): with_mark(K("end")),
     K("C-b"): with_mark(K("left")),
@@ -49,38 +50,36 @@ define_keymap(re.compile("jetbrains-idea"), {
     K("C-p"): with_mark(K("up")),
     K("C-n"): with_mark(K("down")),
     K("C-d"): [K("delete"), set_mark(False)],
-    K("M-d"): [K("C-delete"), set_mark(False)]
+    K("LAlt-d"): [K("C-delete"), set_mark(False)]
     
     # K("C-h"): with_mark(K("backspace"))
-    # K("M-p"): K("C-page_up"),
-    # K("M-n"): K("C-page_down"),
+    # K("LAlt-p"): K("C-page_up"),
+    # K("LAlt-n"): K("C-page_down"),
 })
 
 
 # Keybindings for Firefox/Chrome
-define_keymap(re.compile("Arduino|Firefox|Google-chrome|Slack|rocketchat-desktop|libreoffice|Riot|Postman|chromium|Spectral|Telegram|OpenSCAD|jetbrains-idea"), {
+define_keymap(re.compile("Arduino|Firefox|Google-chrome|Slack|rocketchat-desktop|libreoffice|Riot|Postman|chromium|Spectral|Telegram|OpenSCAD"), {
 
-
-    
     #Ctrl+Alt+j/k to switch next/previous tab
-    K("M-KEY_102ND"): K("C-home"),
-    K("M-LShift-KEY_102ND"): K("C-end"),
-    #K("M-S-MUHENKAN"): K("C-end"),
-    K("M-p"): K("C-page_up"),
-    K("M-n"): K("C-page_down"),
+    K("LAlt-KEY_102ND"): K("C-home"),
+    K("LAlt-LShift-KEY_102ND"): K("C-end"),
+    #K("LAlt-S-MUHENKAN"): K("C-end"),
+    K("LAlt-p"): K("C-page_up"),
+    K("LAlt-n"): K("C-page_down"),
      #Type C-j to focus to the content
     K("C-l"): K("C-f6"),
      #very naive "Edit in editor" feature (just an example)
-    #K("C-M-e"): [K("C-a"), K("C-c"), launch(["emacs"]), sleep(0.5), K("C-v")],
+    #K("C-LAlt-e"): [K("C-a"), K("C-c"), launch(["emacs"]), sleep(0.5), K("C-v")],
     #nowork
 
-    K("M-Shift-p"): K("C-Shift-page_up"),
-    K("M-Shift-n"): K("C-Shift-page_down"),
+    K("LAlt-Shift-p"): K("C-Shift-page_up"),
+    K("LAlt-Shift-n"): K("C-Shift-page_down"),
 
-    K("M-C-p"): K("M-left"),
-    K("M-C-n"): K("M-right"),
+    K("LAlt-C-p"): K("LAlt-left"),
+    K("LAlt-C-n"): K("LAlt-right"),
 
-    K("M-z"): K("C-f4"),
+    K("LAlt-z"): K("C-f4"),
     
         
     K("C-b"): with_mark(K("left")),
@@ -89,31 +88,31 @@ define_keymap(re.compile("Arduino|Firefox|Google-chrome|Slack|rocketchat-desktop
     K("C-n"): with_mark(K("down")),
     K("C-h"): with_mark(K("backspace")),
     # Forward/Backward word
-    K("M-b"): with_mark(K("C-left")),
-    K("M-f"): with_mark(K("C-right")),
+    K("LAlt-b"): with_mark(K("C-left")),
+    K("LAlt-f"): with_mark(K("C-right")),
     # Beginning/End of lineblood flows bet
     K("C-a"): with_mark(K("home")),
     K("C-e"): with_mark(K("end")),
     # Page up/down
-    K("M-v"): with_mark(K("page_up")),
+    K("LAlt-v"): with_mark(K("page_up")),
     K("C-v"): with_mark(K("page_down")),
     # Beginning/End of file
-    K("M-Shift-comma"): with_mark(K("C-home")),
-    K("M-Shift-dot"): with_mark(K("C-end")),
+    K("LAlt-Shift-comma"): with_mark(K("C-home")),
+    K("LAlt-Shift-dot"): with_mark(K("C-end")),
     # Newline
     K("C-m"): K("enter"),
     K("C-j"): K("enter"),
     K("C-o"): [K("enter"), K("left")],
     # Copy
     K("C-w"): [K("C-x"), set_mark(False)],
-    K("M-w"): [K("C-c"), set_mark(False)],
+    K("LAlt-w"): [K("C-c"), set_mark(False)],
     K("C-y"): [K("C-v"), set_mark(False)],
     # Delete
     K("C-d"): [K("delete"), set_mark(False)],
-    K("M-d"): [K("C-delete"), set_mark(False)],
+    K("LAlt-d"): [K("C-delete"), set_mark(False)],
     # Backspace
     K("C-SEMICOLON"): [K("backspace"), set_mark(False)], #semicolon = ö?
-    K("M-SEMICOLON"): [K("C-backspace"), set_mark(False)],
+    K("LAlt-SEMICOLON"): [K("C-backspace"), set_mark(False)],
     # Kill line
     K("C-k"): [K("Shift-end"), K("C-x"), set_mark(False)],
     # Undo
@@ -121,11 +120,11 @@ define_keymap(re.compile("Arduino|Firefox|Google-chrome|Slack|rocketchat-desktop
     K("C-Shift-ro"): K("C-z"),
     # Mark
     K("C-space"): set_mark(True),
-    K("C-M-space"): with_or_set_mark(K("C-right")),
+    K("C-LAlt-space"): with_or_set_mark(K("C-right")),
     # Search
     K("C-s"): [K("F3"), K("C-f")],
     K("C-r"): K("Shift-F3"),
-    K("M-Shift-key_5"): K("C-h"),
+    K("LAlt-Shift-key_5"): K("C-h"),
     # Cancel
     K("C-g"): [K("esc"), set_mark(False)],
     # Escape
@@ -146,9 +145,11 @@ define_keymap(re.compile("Arduino|Firefox|Google-chrome|Slack|rocketchat-desktop
         K("C-g"): pass_through_key,
         # C-x u (undo)
         K("u"): [K("C-z"), set_mark(False)],
+        K("C-b"): K("C-Shift-o"),
+        K("C-h"): [K("C-Shift-h")]
     }
     #K("ALT"): K("reserved")
-    
+
     
 }, "eKeys for stuff + browsery")
 
@@ -167,28 +168,28 @@ define_keymap(re.compile("Arduino|Firefox|Google-chrome|Slack|rocketchat-desktop
 #    K("C-n"): with_mark(K("down")),
 #    K("C-h"): with_mark(K("backspace")),
 #    # Forward/Backward word
-#    K("M-b"): with_mark(K("C-left")),
-#    K("M-f"): with_mark(K("C-right")),
+#    K("LAlt-b"): with_mark(K("C-left")),
+#    K("LAlt-f"): with_mark(K("C-right")),
 #    # Beginning/End of line
 #    K("C-a"): with_mark(K("home")),
 #    K("C-e"): with_mark(K("end")),
 #    # Page up/down
-#    K("M-v"): with_mark(K("page_up")),
+#    K("LAlt-v"): with_mark(K("page_up")),
 #    K("C-v"): with_mark(K("page_down")),
 #    # Beginning/End of file
-#    K("M-Shift-comma"): with_mark(K("C-home")),
-#    K("M-Shift-dot"): with_mark(K("C-end")),
+#    K("LAlt-Shift-comma"): with_mark(K("C-home")),
+#    K("LAlt-Shift-dot"): with_mark(K("C-end")),
 #    # Newline
 #    K("C-m"): K("enter"),
 #    K("C-j"): K("enter"),
 #    K("C-o"): [K("enter"), K("left")],
 #    # Copy
 #    K("C-w"): [K("C-x"), set_mark(False)],
-#    K("M-w"): [K("C-c"), set_mark(False)],
+#    K("LAlt-w"): [K("C-c"), set_mark(False)],
 #    K("C-y"): [K("C-v"), set_mark(False)],
 #    # Delete
 #    K("C-d"): [K("delete"), set_mark(False)],
-#    K("M-d"): [K("C-delete"), set_mark(False)],
+#    K("LAlt-d"): [K("C-delete"), set_mark(False)],
 #    # Kill line
 #    K("C-k"): [K("Shift-end"), K("C-x"), set_mark(False)],
 #    # Undo
@@ -196,11 +197,11 @@ define_keymap(re.compile("Arduino|Firefox|Google-chrome|Slack|rocketchat-desktop
 #    K("C-Shift-ro"): K("C-z"),
 #    # Mark
 #    K("C-space"): set_mark(True),
-#    K("C-M-space"): with_or_set_mark(K("C-right")),
+#    K("C-LAlt-space"): with_or_set_mark(K("C-right")),
 #    # Search
 #    K("C-s"): K("F3"),
 #    K("C-r"): K("Shift-F3"),
-#    K("M-Shift-key_5"): K("C-h"),
+#    K("LAlt-Shift-key_5"): K("C-h"),
 #    # Cancel
 #    K("C-g"): [K("esc"), set_mark(False)],
 #    # Escape
